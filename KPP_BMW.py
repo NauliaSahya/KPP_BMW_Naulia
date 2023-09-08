599798ccd93f9b468e9a3c946243a6c529de75aa
NauliaSahya <115564234+NauliaSahya@users.noreply.github.com>
2023-09-09 00:08:14 +0700

    second commit

--- KPP_BMW.py
+++ KPP_BMW.py
@@ -2,9 +2,9 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.header("KPP BMW")
+st.header("KPP BMW 2023")
 st.subheader("Nama : Naulia Sahya Althafah")
-ecg1 = np.loadtxt('Data ECG.txt', dtype = 'float',)
+ecg1 = np.loadtxt('Data ECG.txt', dtype = 'float')
 Ndata1 = len(ecg1)
 ECG = np.arange(Ndata1)
 st.line_chart(ecg1)
